d073863a016baea3607ab994bd4b4736dd5670b3
lukeku62 (Luca Reverberi) <prevebbo@gmail.com>
2023-10-25 11:28:55 +0200

    aggiunto test somma

--- utils/sum/sum.test.ts
+++ utils/sum/sum.test.ts
@@ -3,4 +3,8 @@ describe("sum", () => {
   it("should sum two numbers", () => {
     expect(sum(1, 2)).toBe(3);
   });
+
+  it("should sum two numbers", () => {
+    expect(sum(-0.5, 7.5)).toBe(7);
+  });
 });
